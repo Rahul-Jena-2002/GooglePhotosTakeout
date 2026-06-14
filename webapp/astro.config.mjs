b0d798041a682d6ee6d237a78f33be0f1f2187e0
Rahul-Jena-2002 <rahuljenasonu@gmail.com>
2026-06-14 18:27:08 +0530

    fix: prerender all SEO dynamic pages to support static build configuration

--- webapp/astro.config.mjs
+++ webapp/astro.config.mjs
@@ -2,16 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 import sentry from '@sentry/astro';
-import cloudflare from '@astrojs/cloudflare';
 import { fileURLToPath } from 'url';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://takeoutfix.pages.dev',
   output: 'static',
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
   integrations: [
     react(),
     tailwind({
