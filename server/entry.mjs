193f8bf68524a5a884394fbd7278b878959669e3
Rahul-Jena-2002 <Rahul-Jena-2002@users.noreply.github.com>
2026-09-26 10:38:59 +0000

    deploy: 475d5bb2b533e4a149e58a8b672175cb7a973877

--- server/entry.mjs
+++ server/entry.mjs
@@ -1,7 +1,7 @@
 globalThis.process ??= {};
 globalThis.process.env ??= {};
 import "cloudflare:workers";
-import { w } from "./chunks/worker-entry_Cx6REkiV.mjs";
+import { w } from "./chunks/worker-entry_BCj3Gkwj.mjs";
 export {
   w as default
 };
